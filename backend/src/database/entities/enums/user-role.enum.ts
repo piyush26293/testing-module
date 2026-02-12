f7a69a8e1ffcc7d74d1748792bb938b3712c735e
Piyush Ahirwar <57410719+piyush26293@users.noreply.github.com>
2026-02-12 10:32:56 +0530

    Create user role enum file

---
+++ backend/src/database/entities/enums/user-role.enum.ts
@@ -0,0 +1,8 @@
+export enum UserRole {
+  ADMIN = 'admin',
+  PROJECT_MANAGER = 'project_manager',
+  QA_LEAD = 'qa_lead',
+  QA_ENGINEER = 'qa_engineer',
+  DEVELOPER = 'developer',
+  VIEWER = 'viewer',
+}
